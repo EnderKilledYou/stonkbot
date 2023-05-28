8938f8768eb275593561961d04bf61ef5f70a25f
Ender <sterlingkooshesh@gmail.com>
2023-05-28 14:43:25 -0500

    Close position

---
+++ src/components/SummaryTab.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+        <b-alert variant="success" show v-if="success">
+            {{ last_success }}
+        </b-alert>
+        <b-alert show variant="danger" v-if="isFetchError">{{ message }}</b-alert>
+        <b-spinner v-if="loading"></b-spinner>
+        <fx-tab :instr="instr" :auth="auth" :column_key="column_key"
+                @cell_click="cell_click" :table="table" @fetch_error="print_error" :button_columns="button_columns"/>
+
+    </div>
+</template>
+<script lang="ts">
+import FxTab from "@/components/FxTab.vue"
+import {Component, Prop, Vue} from "vue-property-decorator";
+import BTableColumnsPicker from "@/components/BTableColumnsPicker.vue";
+import ErrorMessage from "@/views/ErrorMessage.vue";
+import {AuthCredentials} from "@/views/AuthCredentials";
+import PricesTabInstance from "@/components/PricesTabInstance.vue";
+import {column_property} from "@/components/Column_property";
+
+const api = require('../api.js')
+
+
+@Component({
+    components: {PricesTabInstance, FxTab, BTableColumnsPicker, ErrorMessage}
+})
+
+export default class SummaryTab extends Vue {
+    @Prop({default: ''}) table!: string;
+    @Prop() instr!: string;
+    @Prop() auth!: AuthCredentials;
+    column_key = 'offer_id'
+    button_columns: column_property[] = [{'column': 'offer_id', 'column_text': 'Market Close All '}]
+    loading: boolean = false;
+    last_success: string = ''
+    message: string = ''
+    success: boolean = false
+
+    isFetchError: boolean = false;
+
+    cell_click(column: string, value: string, row: any) {
+        debugger;
+        console.log(column)
+        switch (column) {
+            case "offer_id":
+
+                this.close_position(row);
+                break;
+        }
+
+    }
+
+    async close_position(row: any) {
+        try {
+            debugger;
+            this.clear_error();
+            this.loading = true;
+            const result = await api.API.close_instrument(row['instrument'], +row['offer_id'], this.auth.userHash)
+            if (result) {
+                if (result.success) {
+                    debugger;
+                    this.print_success(result.message)
+
+                } else if (result.message) {
+                    this.print_error({message: result.message})
+                }
+
+            } else {
+                this.print_error({message: "Nothing was returned"})
+            }
+        } catch (e: any) {
+            this.print_error({message: e.message})
+
+
+        } finally {
+            this.loading = false;
+        }
+    }
+
+    private clear_error() {
+        this.success = false;
+        this.last_success = '';
+        this.$emit("fetch_error", null)
+
+    }
+
+    private print_success(success: string) {
+        alert(success);
+
+    }
+
+    private print_error(e: any) {
+        this.isFetchError = true;
+        this.message = e.message;
+
+
+    }
+
+
+}
+</script>
